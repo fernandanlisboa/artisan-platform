d0b773ebfeaa4d725d0b64ede9f277e5d6c8e552
Fernanda Lisboa <fernandanlisboa@gmail.com>
2025-06-03 18:14:43 -0300

    add test_connection python script

---
+++ test_connection.py
@@ -0,0 +1,17 @@
+# script_teste_conexao.py
+import sqlalchemy
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+# Adapte para seu dialeto e variáveis
+url = os.getenv("DATABASE_URL")
+print(f"Tentando conectar com: {url}")
+
+try:
+    engine = sqlalchemy.create_engine(url)
+    with engine.connect() as connection:
+        print("Conexão bem-sucedida!")
+except Exception as e:
+    print(f"Falha na conexão: {e}")
