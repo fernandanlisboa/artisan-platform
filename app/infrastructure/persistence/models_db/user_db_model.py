97518439d941169ac71ca3ef0369acbcb75b778d
Fernanda Lisboa <fernandanlisboa@gmail.com>
2025-06-04 17:36:13 -0300

    add init method in UserDBModel

--- app/infrastructure/persistence/models_db/user_db_model.py
+++ app/infrastructure/persistence/models_db/user_db_model.py
@@ -37,6 +37,18 @@ class UserDBModel(db.Model):
     # backref defines the inverse relationships from MessageDBModel
     sent_messages = relationship('MessageDBModel', foreign_keys='[MessageDBModel.sender_user_id]', backref='sender_user')
     received_messages = relationship('MessageDBModel', foreign_keys='[MessageDBModel.recipient_user_id]', backref='recipient_user')
-
+    
+    # --- Método __init__ (Essencial!) ---
+    def __init__(self, email: str, password_hash: str, status: str = 'active', 
+                 user_id: str = None, registration_date: datetime.datetime = None,
+                 address_id: str = None):  # Adicionado address_id
+        self.user_id = user_id if user_id else str(uuid.uuid4())
+        self.email = email
+        self.password_hash = password_hash
+        # self.role = role  # Remover esta linha, role não existe nos parâmetros
+        self.status = status
+        self.registration_date = registration_date if registration_date else datetime.datetime.utcnow()
+        self.address_id = address_id  # Adicionado address_id
+        
     def __repr__(self):
         return f"<UserDBModel(user_id='{self.user_id}', email='{self.email}')>"
