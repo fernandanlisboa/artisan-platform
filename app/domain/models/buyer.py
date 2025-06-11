0c0076b8bd544535dc9e317457dbfa00eccdd2f3
Fernanda Lisboa <fernandanlisboa@gmail.com>
2025-06-11 12:37:30 -0300

    refactor: remove address attribute from BuyerEntity constructor and repr method

--- app/domain/models/buyer.py
+++ app/domain/models/buyer.py
@@ -7,16 +7,15 @@ class BuyerEntity(UserEntity):
     Herda de UserEntity para incluir atributos de usuário comuns como user_id, email, password, status, e address_id.
     """
 
-    def __init__(self, buyer_id: str, full_name: str, address: str, phone: Optional[str] = None):
+    def __init__(self, buyer_id: str, full_name: str, phone: Optional[str] = None):
         """
         Inicializa uma nova instância de BuyerEntity.
         O buyer_id deve ser o mesmo que o user_id da UserEntity correspondente.
         """
         self.buyer_id = buyer_id
         self.full_name = full_name
-        self.address = address
         self.phone = phone        
 
     def __repr__(self):
-        return (f"BuyerEntity(buyer_id='{self.buyer_id}', full_name='{self.full_name}', address='{self.address}' "
+        return (f"BuyerEntity(buyer_id='{self.buyer_id}', full_name='{self.full_name}'"
                 f"phone='{self.phone}')")
