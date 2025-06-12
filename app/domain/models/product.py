18eb5ef50e6a6bf19d55d135b880179b476fcc93
Fernanda Lisboa <fernandanlisboa@gmail.com>
2025-06-12 18:14:46 -0300

    feat: update ProductEntity to include category_id and artisan_id, enhance from_db_model method

--- app/domain/models/product.py
+++ app/domain/models/product.py
@@ -1,5 +1,5 @@
 
-from typing import Any
+from typing import Any, Optional
 from datetime import datetime
 
 class ProductEntity:
@@ -8,15 +8,31 @@ class ProductEntity:
     This class is used to encapsulate product data and can be extended
     """
     
-    def __init__(self, name: str, price: float, stock: int, status: str = "ACTIVE", product_id: str = None, description: str = None, image_url: str = None, registration_date: datetime) -> None:
+    def __init__(
+        self,
+        name: str,
+        price: float,
+        stock: int,
+        category_id: str,
+        status: str = "ACTIVE",
+        product_id: Optional[str] = None,
+        description: Optional[str] = None,
+        image_url: Optional[str] = None,
+        registration_date: Optional[datetime] = None,
+        artisan_id: Optional[str] = None
+    ) -> None:
         self.product_id = product_id
         self.name = name
         self.description = description
         self.price = price
         self.stock = stock
         self.image_url = image_url
-        self.registration_date = registration_date if registration_date else datetime.utcnow()
+        self.registration_date = (
+            registration_date if registration_date else datetime.utcnow()
+        )
         self.status = status
+        self.artisan_id = artisan_id
+        self.category_id = category_id
         
     @classmethod
     def from_db_model(cls, db_model: Any) -> 'ProductEntity':
@@ -32,16 +48,24 @@ class ProductEntity:
             description=db_model.description,
             price=db_model.price,
             stock=db_model.stock,
-            image_url=db_model.image_url,
             registration_date=db_model.registration_date,
-            status=db_model.status
+            status=db_model.status,
+            artisan_id=db_model.artisan_id,
+            image_url=db_model.image_url if db_model.image_url else None,
+            category_id=db_model.category_id
         )
-    
+        
     def __repr__(self) -> str:
-        pass
-    
+        return (
+            f"ProductEntity(product_id={self.product_id!r}, name={self.name!r}, description={self.description!r}, "
+            f"price={self.price!r}, stock={self.stock!r}, image_url={self.image_url!r}, "
+            f"registration_date={self.registration_date!r}, status={self.status!r}, "
+            f"artisan_id={self.artisan_id!r}, category_id={self.category_id!r})"
+        )
+
     def __str__(self) -> str:
-        pass
-    
+        return f"{self.name} ({self.product_id}) - {self.status}"
+
     def __getattribute__(self, name: str) -> Any:
-        pass
+        return object.__getattribute__(self, name)
+        
