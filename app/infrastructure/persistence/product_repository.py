4f74e651fbbef10259d9b76dae705b7e9de779ce
Fernanda Lisboa <fernandanlisboa@gmail.com>
2025-06-12 17:28:56 -0300

    refactor: implement product creation logic in create_artisan_product method

--- app/infrastructure/persistence/product_repository.py
+++ app/infrastructure/persistence/product_repository.py
@@ -12,24 +12,24 @@ class ProductRepository(IProductRepository):
         Saves a Product entity to the database.
         Converts the pure domain entity to a database model before saving.
         """
-        pass
-        # # CONVERSION: Pure Domain Entity -> ORM Model
-        # product_db_model = ProductDBModel(
-        #     product_id=product_entity.product_id,
-        #     name=product_entity.name,
-        #     description=product_entity.description,
-        #     price=product_entity.price,
-        #     stock=product_entity.stock,
-        #     artisan_id=product_entity.artisan_id
-        # )
         
-        # print("Product DB Model: ", product_db_model)
-        # try:
-        #     db.session.add(product_db_model)
-        #     db.session.commit()
-        # except Exception as e:
-        #     print(f"Error saving product: {e}")
-        #     db.session.rollback()
+        # CONVERSION: Pure Domain Entity -> ORM Model
+        product_db_model = ProductDBModel(
+            product_id=product_entity.product_id,
+            name=product_entity.name,
+            description=product_entity.description,
+            price=product_entity.price,
+            stock=product_entity.stock,
+            artisan_id=product_entity.artisan_id
+        )
         
-        # print("Product Entity after save: ", product_entity)
-        # return product_entity  # Retorna a entidade pura que foi salva
+        print("Product DB Model: ", product_db_model)
+        try:
+            db.session.add(product_db_model)
+            db.session.commit()
+        except Exception as e:
+            print(f"Error saving product: {e}")
+            db.session.rollback()
+        
+        print("Product Entity after save: ", product_entity)
+        return product_entity  # Retorna a entidade pura que foi salva
