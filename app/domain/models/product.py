4f1a077ec2f5d8d01227a451b06a313940616993
Fernanda Lisboa <fernandanlisboa@gmail.com>
2025-06-12 12:39:10 -0300

    feat: add ProductEntity class to encapsulate product data

---
+++ app/domain/models/product.py
@@ -0,0 +1,21 @@
+
+from typing import Any
+
+
+class ProductEntity:
+    """
+    Represents a product entity with its attributes.
+    This class is used to encapsulate product data and can be extended
+    """
+    
+    def __init__(self) -> None:
+        pass
+    
+    def __repr__(self) -> str:
+        pass
+    
+    def __str__(self) -> str:
+        pass
+    
+    def __getattribute__(self, name: str) -> Any:
+        pass
