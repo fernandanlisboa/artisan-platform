39c10a095df533b0c70e3d954ebd3517a6e6304b
Fernanda Lisboa <fernandanlisboa@gmail.com>
2025-06-17 18:23:03 -0300

    feat: add artisan namespace to API and integrate artisan product management

--- app/presentation/controllers/artisan_controller.py
+++ app/presentation/controllers/artisan_controller.py
@@ -6,29 +6,36 @@ from pydantic import ValidationError
 from flask_restx import Namespace, Resource, fields
 
 from app.application.services.artisan_product_service import ArtisanProductService
+from app.infrastructure.persistence.artisan_repository import ArtisanRepository
+from app.infrastructure.persistence.category_repository import CategoryRepository
 from app.infrastructure.persistence.product_repository import ProductRepository
 from app.presentation.dtos.product_dtos import RegisterProductRequest
 
 
 artisan_product_service_instance = ArtisanProductService(
-    product_repository=  ProductRepository()
+    product_repository=ProductRepository(),
+    category_repository=CategoryRepository(),
+    artisan_repository=ArtisanRepository()
     )
-#TODO: adicionar autenticação e autorização
-api = Namespace('artisan', description='Artisan management operations')
 
-@api.route('/<string:artisan_id>/product')
+#TODO: adicionar autenticação e autorização
+artisan_ns = Namespace('artisan', description='Artisan management operations')
+
+@artisan_ns.route('/<string:artisan_id>/product')
 class ArtisanProductResource(Resource):
     """
     Resource for managing artisan products.
     """
     artisan_product_service = artisan_product_service_instance
 
-    @api.doc('create_artisan_product')
-    @api.expect(api.model('Product', {
+    @artisan_ns.doc('create_artisan_product')
+    @artisan_ns.expect(artisan_ns.model('Product', {
         'name': fields.String(required=True, description='Name of the product'),
         'description': fields.String(required=True, description='Description of the product'),
         'price': fields.Float(required=True, description='Price of the product'),
-        'stock': fields.Integer(required=True, description='Stock quantity of the product')
+        'stock': fields.Integer(required=True, description='Stock quantity of the product'),
+        'category_id': fields.String(required=True, description='Category ID of the product'),
+        'image_url': fields.String(required=False, description='Image URL of the product')
     }))
     def post(self, artisan_id):
         """
@@ -36,19 +43,19 @@ class ArtisanProductResource(Resource):
         """
         try:
             
-            request_data = RegisterProductRequest(**api.payload)
+            request_data = RegisterProductRequest(**artisan_ns.payload)
             created_product = artisan_product_service_instance.create_artisan_product(
                 artisan_id=artisan_id,
                 product_data=request_data
             )
             
-            return created_product, 201  # Return the created product with HTTP status 201
+            return created_product.model_dump(), 201  # Return the created product with HTTP status 201
         except ValidationError as e:
-            api.abort(400, str(e))
+            artisan_ns.abort(400, str(e))
 
         except ValueError as e:
-            api.abort(400, str(e))
+            artisan_ns.abort(400, str(e))
         
         except Exception as e:
             print(f"Internal server error during buyer registration: {e}")
-            api.abort(500, "Internal server error")
+            artisan_ns.abort(500, "Internal server error")
