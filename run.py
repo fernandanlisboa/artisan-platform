0d7910fb8c519fa9612b45f2c3d78b5a12917e33
Fernanda Lisboa <fernandanlisboa@gmail.com>
2025-06-27 22:16:24 -0300

    Bind Flask app to host '0.0.0.0' and dynamic port from environment variable

--- run.py
+++ run.py
@@ -6,4 +6,9 @@ app = create_app()
 
 
 if __name__ == '__main__':
-    app.run(debug=os.getenv('FLASK_DEBUG', 'True') == 'True')
+    port = int(os.environ.get('PORT', 8080))
+    
+    # Executa a aplicação Flask, vinculando-a ao host '0.0.0.0' e à porta dinâmica.
+    # 'debug=os.getenv('FLASK_DEBUG', 'True') == 'True'' mantém sua lógica de debug.
+    app.run(host='0.0.0.0', port=port, debug=os.getenv('FLASK_DEBUG', 'True') == 'True')
+
