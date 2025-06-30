88f5af971e6b3645747f39b692300dc0d030f861
Fernanda Lisboa <fernandanlisboa@gmail.com>
2025-06-29 21:36:52 -0300

    Refactor Dockerfile and run.py for Gunicorn deployment; update requirements.txt to include gunicorn

--- run.py
+++ run.py
@@ -2,13 +2,11 @@
 from app import create_app
 import os
 
+# O Gunicorn vai procurar por esta variável 'app' por padrão.
 app = create_app()
 
-
+# O bloco abaixo agora será usado apenas para desenvolvimento local
+# e será ignorado pelo Gunicorn no contêiner.
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 8080))
-    
-    # Executa a aplicação Flask, vinculando-a ao host '0.0.0.0' e à porta dinâmica.
-    # 'debug=os.getenv('FLASK_DEBUG', 'True') == 'True'' mantém sua lógica de debug.
-    app.run(host='0.0.0.0', port=port, debug=os.getenv('FLASK_DEBUG', 'True') == 'True')
-
+    app.run(host='0.0.0.0', port=port, debug=True)
